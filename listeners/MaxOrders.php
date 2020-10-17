bdee0326be7844e6bf72e8d8e615d2c4f2bee857
Ryan Mitchell <ryan@thoughtcollective.com>
2020-10-17 16:52:04 +0100

    Add covers count to listener

--- listeners/MaxOrders.php
+++ listeners/MaxOrders.php
@@ -2,6 +2,7 @@
 
 namespace Thoughtco\Maxorders\Listeners;
 
+use Admin\Models\Menus_model;
 use Admin\Models\Orders_model;
 use Carbon\Carbon;
 use Igniter\Flame\Location\Models\AbstractLocation;
@@ -15,7 +16,8 @@ class MaxOrders
     use EventEmitter;
 
     protected static $ordersCache = [];
-
+    protected static $menusCache = [];
+    
     public function subscribe(Dispatcher $dispatcher)
     {
         $dispatcher->listen('igniter.workingSchedule.timeslotValid', __CLASS__.'@timeslotValid');
@@ -37,36 +39,62 @@ class MaxOrders
         $startTime = Carbon::parse($timeslot);
         $endTime = Carbon::parse($timeslot)->addMinutes($locationModel->getOrderTimeInterval($workingSchedule->getType()));
         
-        $maxCount = $locationModel->getOption('limit_orders_count');
+        $removeSlot = false;
         
         Timeslots::where([
             ['location_id', $locationModel->location_id],
             ['timeslot_status', 1],            
         ])
-        ->each(function($limitation) use ($dayOfWeek, $startTime, &$maxCount){
+        ->each(function($limitation) use (&$removeSlot, $dayOfWeek, $startTime, $endTime, $ordersOnThisDay){
             if (in_array($dayOfWeek, $limitation->timeslot_day))
             {
                 $limitationStart = Carbon::createFromFormat('Y-m-d H:i:s', $startTime->format('Y-m-d').' '.$limitation->timeslot_start);
                 $limitationEnd = Carbon::createFromFormat('Y-m-d H:i:s', $startTime->format('Y-m-d').' '.$limitation->timeslot_end);
                 if ($startTime->between($limitationStart, $limitationEnd))
                 {
-                    $maxCount = $limitation->timeslot_max;      
+                    $ordersOnThisDay = $ordersOnThisDay
+                    ->filter(function ($order) use ($startTime, $endTime) {
+                        $orderTime = Carbon::createFromFormat('Y-m-d H:i:s', $startTime->format('Y-m-d').' '.$order->order_time);
+            
+                        return $orderTime->between(
+                            $startTime,
+                            $endTime
+                        );
+                    });
+                                        
+                    // if limiting by categories then we need to count up the number of items
+                    // in the categories
+                    if ($limitation->timeslot_max_type == 'covers')
+                    {
+                        $ordersOnThisDay = $ordersOnThisDay->map(function($order) use ($limitation) {
+                            $myCount = 0;
+                            $order->getOrderMenus()
+                            ->each(function($orderMenu) use ($limitation, &$myCount) {
+                                $intersect = $this->getMenuCategories($orderMenu->menu_id)->intersect($limitation->timeslot_categories);
+                                if ($intersect->count())
+                                {
+                                    $myCount += $orderMenu->quantity;
+                                }
+                            });
+                            return $myCount;
+                        });             
+
+                        // get sum of all covers
+                        $orderCount = $ordersOnThisDay->sum();
+                     
+                    // otherwise we count orders on this day   
+                    } else {
+                        $orderCount = $ordersOnThisDay->count();
+                    }
+                    
+                    if ($orderCount >= $limitation->timeslot_max)
+                        $removeSlot = true;
                 }
             }
         });
 
-        $orderCount = $ordersOnThisDay->filter(function ($time) use ($startTime, $endTime) {
-            $orderTime = Carbon::createFromFormat('Y-m-d H:i:s', $startTime->format('Y-m-d').' '.$time);
-
-            return $orderTime->between(
-                $startTime,
-                $endTime
-            );
-        });
-
-        if ($orderCount->count() >= $maxCount)
+        if ($removeSlot)
             return FALSE;
-
     }
 
     protected function getOrders($date)
@@ -74,12 +102,25 @@ class MaxOrders
         if (array_has(self::$ordersCache, $date))
             return self::$ordersCache[$date];
 
-        $result = Orders_model::where('order_date', $date)
-            ->where('location_id', LocationFacade::getId())
+        $result = Orders_model::where('location_id', LocationFacade::getId())
+            ->where('order_date', $date)
             ->whereIn('status_id', array_merge(setting('processing_order_status', []), setting('completed_order_status', [])))
-            ->select(['order_time'])
-            ->pluck('order_time');
+            ->get();
 
         return self::$ordersCache[$date] = $result;
     }
+    
+    protected function getMenuCategories($menuId)
+    {
+        if (array_has(self::$menusCache, $menuId))
+            return self::$menusCache[$menuId];
+
+        $result = Menus_model::where('menu_id', $menuId)
+            ->first()
+            ->categories()
+            ->pluck('categories.category_id');
+
+        return self::$menusCache[$menuId] = $result;
+    }
+    
 }
